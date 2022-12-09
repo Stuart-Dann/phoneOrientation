489341c92c01b6144e3a0e8b26b2a09b5fa4f125
Thrgamer <stuartdann03@gmail.com>
2022-12-09 13:48:44 +0000

    finally

--- orientation.js
+++ orientation.js
@@ -2,8 +2,8 @@ function updateOrientation(e) {
 	a = document.querySelector('#alpha')
 	b = document.querySelector('#beta')
 	g = document.querySelector('#gamma')
-	a.innerHTML = e.alpha
-	b.innerHTML = e.beta
-	g.value = e.gamma
+	a.textContent = e.alpha
+	b.textContent = e.beta
+	g.textContent = e.gamma
 }
-window.addEventListener('deviceorientation', updateOrientation);
+window.addEventListener('deviceorientation', updateOrientation)
